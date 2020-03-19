e1b708629e06e17b881d2068e74689f58ca689e7
Lord Tapulao <55147167+LTapsy@users.noreply.github.com>
2020-03-18 22:55:29 -0600

    commit

--- www/js/recovered.js
+++ www/js/recovered.js
@@ -40,13 +40,13 @@ function initialLoad(){
 
         // for(x = 0; x!= covid.locations.length;x++){
         for(x = 0; x!= covid.locations.length;x++){
-            // console.log(covid.locations[x].province);
-            if(covid.locations[x].province == ""){
-                locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
-            }else{
-                locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].province+", "+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
+            if(covid.locations[x].latest != "0"){
+                if(covid.locations[x].province == ""){
+                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
+                }else{
+                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].province+", "+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
+                }
             }
-            
         }
     
         $(".locationContainer").html(locations);
