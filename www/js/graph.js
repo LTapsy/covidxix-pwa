5cd1ebae84c8c624489409b5ed8e2287e2df26f6
Lord Tapulao <55147167+LTapsy@users.noreply.github.com>
2020-03-21 03:33:28 -0600

    commit

--- www/js/graph.js
+++ www/js/graph.js
@@ -1,146 +1,93 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: ['4 days ago', '3 days ago', '2 days ago', 'Yesterday', 'Today'],
-                datasets: [{
-                    label: 'Corona Updates',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)'
-                    ],
-                    borderWidth: 1
-                },
-                {
-                    label: 'Corona Updates',
-                    data: [3, 4, 13, 15, 22, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function initialLoad(){
+
+    fetch("https://cors-anywhere.herokuapp.com/" + "https://coronavirus-tracker-api.herokuapp.com/all", {
+        "method": "GET"
+    })
+    .then(res => res.json())          
+    .then(covid => {
+        
+        
+
+        var d = new Date();
+        var yr = JSON.stringify(d.getFullYear());
+        var year = yr.slice(2, 4);
+        var buwan = String(d.getMonth() + 1).padStart(2, '0');
+        var month;
+        if(buwan.slice(0, 1)==0){
+            month = buwan.slice(1, 2)
+        }else{
+            month = buwan
+        }
+
+        var day = String(d.getDate()).padStart(2, '0');
+        
+        var date = month +"/"+ day +"/"+ year;
+        console.log(date)   
+        console.log(covid.confirmed.locations[0].history);
+
+        date = month +"/"+ day +"/"+ year;
+        value = covid.confirmed.locations[0].history[date] / 1.5;
+        cx.lineTo(0,value);
+        
+        for(x=0;x!=20;x++){
+            date = month +"/"+ day +"/"+ year;
+            console.log(day)
+            value = covid.confirmed.locations[0].history[date] / 1.5;
+            console.log(value);
+            cx.lineTo(path,value);
+            path+=space;
+            day--;
+            if(day==0){
+                month--;
+                day=29;
             }
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function initialLoad(){
-
-//     fetch("https://cors-anywhere.herokuapp.com/" + "https://coronavirus-tracker-api.herokuapp.com/all", {
-//         "method": "GET"
-//     })
-//     .then(res => res.json())          
-//     .then(covid => {
-        
-        
-
-//         var d = new Date();
-//         var yr = JSON.stringify(d.getFullYear());
-//         var year = yr.slice(2, 4);
-//         var buwan = String(d.getMonth() + 1).padStart(2, '0');
-//         var month;
-//         if(buwan.slice(0, 1)==0){
-//             month = buwan.slice(1, 2)
-//         }else{
-//             month = buwan
-//         }
-
-//         var day = String(d.getDate()).padStart(2, '0');
-        
-//         var date = month +"/"+ day +"/"+ year;
-//         console.log(date)   
-//         console.log(covid.confirmed.locations[0].history);
-
-//         date = month +"/"+ day +"/"+ year;
-//         value = covid.confirmed.locations[0].history[date] / 1.5;
-//         cx.lineTo(0,value);
-        
-//         for(x=0;x!=20;x++){
-//             date = month +"/"+ day +"/"+ year;
-//             console.log(day)
-//             value = covid.confirmed.locations[0].history[date] / 1.5;
-//             console.log(value);
-//             cx.lineTo(path,value);
-//             path+=space;
-//             day--;
-//             if(day==0){
-//                 month--;
-//                 day=29;
-//             }
-//         }
-//         cx.lineTo(100000000,0)
+        }
+        cx.lineTo(100000000,0)
         
 
         
 
 
 
-// cx.lineWidth = 1;
-// cx.strokeStyle = "dodgerblue";
-// cx.stroke();
+cx.lineWidth = 1;
+cx.strokeStyle = "dodgerblue";
+cx.stroke();
 
 
 
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     });
+    })
+    .catch(err => {
+        console.log(err);
+    });
     
     
     
-// }initialLoad();
+}initialLoad();
--- www/js/sideMenu.js
+++ www/js/sideMenu.js
@@ -9,7 +9,7 @@ SideMenu = () => {
     var except = document.querySelector('menu');
 
     body.addEventListener("click", function () {
-        sideMenu.style = "opacity:1;transform: translateX(-100vw);transition: 0.4s ease-out;";
+        sideMenu.style = "opacity:0;transform: translateX(-100vw);transition: 0.4s ease-out;";
     }, false);
     except.addEventListener("click", function (ev) {
         ev.stopPropagation(); 
