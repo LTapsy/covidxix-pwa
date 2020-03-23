7d80334621e1003446eea4e5bc85ce818a121e71
Lord Tapulao <55147167+LTapsy@users.noreply.github.com>
2020-03-22 18:31:30 -0600

    landing page added

--- platforms/android/app/src/main/assets/www/js/confirmed.js
+++ platforms/android/app/src/main/assets/www/js/confirmed.js
@@ -1,4 +1,6 @@
 var coronaV
+arr = [];
+result = [];
 function initialLoad(){
     var covid
     fetch("https://cors-anywhere.herokuapp.com/" + "https://coronavirus-tracker-api.herokuapp.com/confirmed", {
@@ -45,57 +47,35 @@ function initialLoad(){
             // console.log(covid.locations[x].province);
             if(covid.locations[x].latest != "0"){
                 if(covid.locations[x].province == ""){
-                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p><button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#myModal">Open Modal</button></div>`;
+                    locations += `<div class="locations" data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
                 }else{
-                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].province+", "+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p><button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#myModal">Open Modal</button></div>`;
+                    locations += `<div class="locations"  data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].province+", "+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
                 }
             }
             var ass = covid.locations[x].history
-            var ctxLine = document.getElementById("myChart").getContext("2d");
-            if(window.bar != undefined) 
-            window.bar.destroy(); 
-            window.bar = new Chart(ctxLine, {
-                type: 'bar',
-                data: {
-                    labels: Object.keys(ass),
-                    datasets: [{
-                        label: '# of Votes',
-                        data: Object.values(ass),
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
             
-            });
-
+            for(i=0; i != 59 ;  i++){
+                arr.push({"date":Object.keys(ass)[i], "number" : Object.values(ass)[i]}) //this is whre i reconstruct the array thatt the api gave us, because before it was date: number, i changed it to date: date, number: number
+            }
         }
-    
+        function chunkArray(myArray, chunk_size){ //this is where i divide the whole array which is 20k+ by 59 so each contry has its own formatted array of date and numbers
+            var index = 0;
+            var arrayLength = arr.length;
+            var tempArray = [];
+            
+            for (index = 0; index < arrayLength; index += chunk_size) {
+                myChunk = myArray.slice(index, index+chunk_size);
+                // Do something if you want with the group
+                tempArray.push(myChunk);
+            }
+        
+            return tempArray;
+        }
+        result = chunkArray(arr, 59); //existing array that is divided by 59
+        
         $(".locationContainer").html(locations);
         $(".loadingScreen").hide();
+        
     })
     .catch(err => {
         console.log(err);
@@ -120,9 +100,9 @@ function regenerate(){
         // console.log(covid.locations[x].province);
         
             if(filtered[x].province == ""){
-                locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+filtered[x].country+`</p><p class="caseCount">Count: `+filtered[x].latest+`</p></div>`;
+                locations += `<div class="locations"  data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+filtered[x].country+`</p><p class="caseCount">Count: `+filtered[x].latest+`</p></div>`;
             }else{
-                locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+filtered[x].province+", "+filtered[x].country+`</p><p class="caseCount">Count: `+filtered[x].latest+`</p></div>`;
+                locations += `<div class="locations"  data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+filtered[x].province+", "+filtered[x].country+`</p><p class="caseCount">Count: `+filtered[x].latest+`</p></div>`;
             }
         }
         
@@ -131,12 +111,66 @@ function regenerate(){
         for(x = 0; x!= coronaV.locations.length;x++){
         // console.log(covid.locations[x].province);
             if(coronaV.locations[x].province == ""){
-                locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+coronaV.locations[x].country+`</p><p class="caseCount">Count: `+coronaV.locations[x].latest+`</p></div>`;
+                locations += `<div class="locations"  data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+coronaV.locations[x].country+`</p><p class="caseCount">Count: `+coronaV.locations[x].latest+`</p></div>`;
             }else{
-                locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+coronaV.locations[x].province+", "+coronaV.locations[x].country+`</p><p class="caseCount">Count: `+coronaV.locations[x].latest+`</p></div>`;
+                locations += `<div class="locations"  data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+coronaV.locations[x].province+", "+coronaV.locations[x].country+`</p><p class="caseCount">Count: `+coronaV.locations[x].latest+`</p>></div>`;
             }
         }
     }
     $("#locationContainer").html("")
     $(".locationContainer").html(locations);
+}
+function renderChart(arrayNumber){
+    var arrs = []
+    var arrss = []
+    var newarr = [];
+    for(g=0;g!= 59; g++){
+        newarr.push('rgba(255, 99, 132, 0.2)')
+    }
+    function compare(a,b){
+        const dateA = new Date(a.date) //first i sort the array of 59 date and number, because on the api, the date is unorganized.
+        const dateB = new Date(b.date)
+        let comparison = 0;
+
+        if(dateA > dateB){
+            comparison = -1;
+        }else if(dateA < dateB){
+            comparison = 1;
+        }
+        return comparison * -1;
+    }
+    result[arrayNumber].sort(compare)
+    
+    // Outputs : [ [1,2,3] , [4,5,6] ,[7,8] ]
+    result[arrayNumber].forEach(function(element) { //this is where i render the chart based on which country button is clicked
+        arrs.push(element.date) //this is where i generate the date into seperate array
+        arrss.push(element.number); // and seperete array for numbers aswell to be brought to the data and label of chartJS
+        var ctxLine = document.getElementById("historyChart").getContext("2d");
+        if(window.bar != undefined) 
+        window.bar.destroy(); 
+        window.bar = new Chart(ctxLine, {
+            type: 'bar',
+            data: {
+                labels: arrs,
+                datasets: [{
+                    label: '# of Confirmed Cases',
+                    data: arrss,
+                    backgroundColor: newarr,
+                    borderColor: newarr,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        
+        });
+        
+    })
 }
--- platforms/android/app/src/main/assets/www/js/recovered.js
+++ platforms/android/app/src/main/assets/www/js/recovered.js
@@ -1,5 +1,8 @@
 var coronaV
+arr = [];
+result = [];
 function initialLoad(){
+
     var covid
     fetch("https://cors-anywhere.herokuapp.com/" + "https://coronavirus-tracker-api.herokuapp.com/recovered", {
         "method": "GET"
@@ -43,13 +46,32 @@ function initialLoad(){
         for(x = 0; x!= covid.locations.length;x++){
             if(covid.locations[x].latest != "0"){
                 if(covid.locations[x].province == ""){
-                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
+                    locations += `<div class="locations" data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
                 }else{
-                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].province+", "+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
+                    locations += `<div class="locations" data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+covid.locations[x].province+", "+covid.locations[x].country+`</p><p class="caseCount">Count: `+covid.locations[x].latest+`</p></div>`;
                 }
             }
+            var ass = covid.locations[x].history
+            
+            for(i=0; i != 59 ;  i++){
+                arr.push({"date":Object.keys(ass)[i], "number" : Object.values(ass)[i]})
+            }
         }
-    
+        function chunkArray(myArray, chunk_size){
+            var index = 0;
+            var arrayLength = arr.length;
+            var tempArray = [];
+            
+            for (index = 0; index < arrayLength; index += chunk_size) {
+                myChunk = myArray.slice(index, index+chunk_size);
+                // Do something if you want with the group
+                tempArray.push(myChunk);
+            }
+        
+            return tempArray;
+        }
+        result = chunkArray(arr, 59);
+
         $(".locationContainer").html(locations);
         $(".loadingScreen").hide();
     })
@@ -60,8 +82,6 @@ function initialLoad(){
     
     
     }initialLoad();
-    
-    
     
     
     function regenerate(){
@@ -77,9 +97,9 @@ function initialLoad(){
             // console.log(covid.locations[x].province);
             
                 if(filtered[x].province == ""){
-                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+filtered[x].country+`</p><p class="caseCount">Count: `+filtered[x].latest+`</p></div>`;
+                    locations += `<div class="locations" data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+filtered[x].country+`</p><p class="caseCount">Count: `+filtered[x].latest+`</p></div>`;
                 }else{
-                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+filtered[x].province+", "+filtered[x].country+`</p><p class="caseCount">Count: `+filtered[x].latest+`</p></div>`;
+                    locations += `<div class="locations" data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+filtered[x].province+", "+filtered[x].country+`</p><p class="caseCount">Count: `+filtered[x].latest+`</p></div>`;
                 }
             }
             
@@ -88,13 +108,65 @@ function initialLoad(){
             for(x = 0; x!= coronaV.locations.length;x++){
             // console.log(covid.locations[x].province);
                 if(coronaV.locations[x].province == ""){
-                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+coronaV.locations[x].country+`</p><p class="caseCount">Count: `+coronaV.locations[x].latest+`</p></div>`;
+                    locations += `<div class="locations" data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+coronaV.locations[x].country+`</p><p class="caseCount">Count: `+coronaV.locations[x].latest+`</p></div>`;
                 }else{
-                    locations += `<div class="locations"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+coronaV.locations[x].province+", "+coronaV.locations[x].country+`</p><p class="caseCount">Count: `+coronaV.locations[x].latest+`</p></div>`;
+                    locations += `<div class="locations" data-toggle="modal" data-target="#myModal" onclick="renderChart(`+ x +`)"><i class="fas fa-biohazard"></i><p  class="pwestuhan">`+coronaV.locations[x].province+", "+coronaV.locations[x].country+`</p><p class="caseCount">Count: `+coronaV.locations[x].latest+`</p></div>`;
                 }
             }
         }
         $("#locationContainer").html("")
         $(".locationContainer").html(locations);
     }
+    function renderChart(arrayNumber){
+        var arrs = []
+        var arrss = []
+        var newarr = [];
+        for(g=0;g!= 59; g++){
+            newarr.push('rgba(255, 99, 132, 0.2)')
+        }
+        function compare(a,b){
+            const dateA = new Date(a.date)
+            const dateB = new Date(b.date)
+            let comparison = 0;
     
+            if(dateA > dateB){
+                comparison = -1;
+            }else if(dateA < dateB){
+                comparison = 1;
+            }
+            return comparison * -1;
+        }
+        result[arrayNumber].sort(compare)
+        
+        // Outputs : [ [1,2,3] , [4,5,6] ,[7,8] ]
+        result[arrayNumber].forEach(function(element) {
+            arrs.push(element.date)
+            arrss.push(element.number);
+            var ctxLine = document.getElementById("historyChart").getContext("2d");
+            if(window.bar != undefined) 
+            window.bar.destroy(); 
+            window.bar = new Chart(ctxLine, {
+                type: 'bar',
+                data: {
+                    labels: arrs,
+                    datasets: [{
+                        label: '# of Confirmed Recoveries',
+                        data: arrss,
+                        backgroundColor: newarr,
+                        borderColor: newarr,
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            
+            });
+        })
+    }
